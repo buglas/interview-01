2874b2af6136ffc779707f7b06e6379bca5613ad
buglas <liwei1051904257@gmail.com>
2020-09-28 16:45:14 +0800

    imgObj

--- jsm/shapeLib/Rect.js
+++ jsm/shapeLib/Rect.js
@@ -3,33 +3,42 @@ import Vector2 from "../core/Vector2.js"
 import Shape from "./Shape.js"
 
 /*Rect 矩形对象-普通图形|晶格化修改器节点
-*   single 静态属性，是否一线一点，可默认undefined
 *   orign 定原点,可为center，基点在中心
-*   width 宽度，默认0
-*   height 高度，默认0
+*   size 尺寸
 * */
 
 export default class Rect extends Shape{
     constructor(...attrs) {
-        super(...attrs,{width:0,height:0,close:true});
+        super(...attrs,{
+            size:new Vector2(),
+            min:new Vector2(),
+            close:true
+        });
         this.update();
     }
     update() {
-        const {orign,width,height}=this;
-        let x=0;
-        let y=0;
-        if(orign==='center'){
-            x=width/2;
-            y=height/2;
-        }else {
-            x=orign.x;
-            y=orign.y;
-        }
+        const {min,size:{x:w,y:h}}=this;
+        const {x:l,y:t}=this.getMin();
         this.vertices=[
-            new Vector2(-x,-y),
-            new Vector2(width-x,-y),
-            new Vector2(width-x,height-y),
-            new Vector2(-x,height-y),
+            new Vector2(-l,-t),
+            new Vector2(w-l,-t),
+            new Vector2(w-l,h-t),
+            new Vector2(-l,h-t),
         ]
+        min.x=l;
+        min.y=t;
+    }
+    getMin(){
+        const {min,orign,size:{x:w,y:h}}=this;
+        let l=0;
+        let t=0;
+        if(orign==='center'){
+            l=w/2;
+            t=h/2;
+        }else {
+            l=orign.x;
+            t=orign.y;
+        }
+        return new Vector2(l,t);
     }
 }
